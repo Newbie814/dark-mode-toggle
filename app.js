6471e0d2d0402dad210898423b4ce588b72699db
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-04-07 14:12:50 -0400

    added dynamic content loading

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const toggleDark = document.querySelector('.btn')
+const articlesContainer = document.querySelector('.articles')
+
+const articlesDatabase = articles.map((article) => {
+  const { title, date, length, preview} = article
+
+  return `
+  <article class="post">
+           <h2>${title}</h2>
+           <div class="post-info">
+             <span>${date}</span>
+             <span>${length} min read</span>
+           </div>
+           <p>${preview}</p>
+         </article>`
+})
+.join('')
 
 toggleDark.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-mode')
+
+
 })
+
+articlesContainer.innerHTML = articlesDatabase
+
+
+
---
+++ articles.js
@@ -0,0 +1,30 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Why Real Food?',
+    date: new Date(2022, 3, 15),
+    length: 8,
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus deserunt ullam dolorem ipsum! Consequuntur consequatur veniam delectus maiores earum rem, mollitia eveniet aut iure molestias incidunt doloribus labore est. Provident.'
+  },
+  {
+    id: 2,
+    title: 'It\'s Not Magic',
+    date: new Date(202, 12, 15),
+    length: 12,
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus deserunt ullam dolorem ipsum! Consequuntur consequatur veniam delectus maiores earum rem, mollitia eveniet aut iure molestias incidunt doloribus labore est. Provident.'
+  },
+  {
+    id: 3,
+    title: 'It\'s Damn Sure Not A Religion.',
+    date: new Date(2022, 1, 15),
+    length: 8,
+    preview: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus deserunt ullam dolorem ipsum! Consequuntur consequatur veniam delectus maiores earum rem, mollitia eveniet aut iure molestias incidunt doloribus labore est. Provident.'
+  },
+  {
+    id: 4,
+    title: 'You Can Lose Weight With Exercise Alone.',
+    date: new Date(2022, 3, 1),
+    length: 8,
+    preview: 'April Fool\'s dolor sit amet, consectetur adipisicing elit. Natus deserunt ullam dolorem ipsum! Consequuntur consequatur veniam delectus maiores earum rem, mollitia eveniet aut iure molestias incidunt doloribus labore est. Provident.'
+  },
+]
